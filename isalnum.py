3d0843a4dcdc1ce12e6b562e1bd2ced96cb00730
himanshuvaishya170 <148369894+himanshuvaishya170@users.noreply.github.com>
2024-02-03 01:38:22 +0530

    Add files via upload

---
+++ isalnum.py
@@ -0,0 +1,6 @@
+a="abc123"
+print(a.isalnum())
+b="123221"
+print(b.isdigit())
+c="  "
+print(c.isspace())
---
+++ strpallin.py
@@ -0,0 +1,6 @@
+n=input("enter name")
+b=n[-1::-1]
+if(n==b):
+    print("pallindrome")
+else :
+    print("not palllindrome")    
